b61d3401313a976095f49e999587c1d9629dfe19
kzkysu <maja.orlow@gmail.com>
2018-12-23 05:49:38 -0500

    commi-msg probuje wpisac tekst z wejscia i wypisuje co wczytalo

--- commit-msg.c
+++ commit-msg.c
@@ -5,15 +5,30 @@
 #include <stdio.h>
 
 const int maxdl = 50;
+char opis[1000];
 
 int main(int argc, char *argv[])
 {
-	int dl=strlen(argv[1]);
+	//int dl=strlen(argv[2]);
 //	printf("%d", dl);
+	int ch=0;
+	int dl=0; 
+	while((ch =  getchar())!=EOF && dl<1000 )
+	{
+		opis[dl]=ch;
+		dl++;
+	}
+	printf("\n\n");
 	if( dl > maxdl )
 	{
 		printf("Zablokowano commit - dlugosc opisu %d maksymalna dlugosc opisu %d\n",dl, maxdl);
 		return 1;
 	}
+	else printf("Dlugosc opisu odpowiednia: %d\n", dl );
+	for( int i=0;i<dl; i++ )
+	{
+		printf("%c",argv[0][i]);
+	}
+	printf("\n\n");
 	return 0; 
 }
